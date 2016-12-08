c5d2d00738b5e2255e0d08af0099e8348bec3325
keylinjay <k547531366@hotmail.com>
2016-12-08 22:10:03 +0800

    fix conflict from macair

--- html-demo/012.自适应菜单/my.js
+++
@@ -1,9 +0,0 @@
-var nav = document.getElementsByClassName('in'),
-	bn = document.getElementsByClassName("bn");
-
-bn[0].onclick=function(e) {
-	nav[0].style.height===""?nav[0].style.height="auto":nav[0].style.height="";
-};
-bn[1].onclick=function(e) {
-	nav[1].style.height===""?nav[1].style.height="8em":nav[1].style.height="";
-};
